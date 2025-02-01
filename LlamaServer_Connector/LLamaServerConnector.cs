e59f0181a6ae8b79f82c7b5e157bbc9ecb9cbf11
info@vltmedia.com <info@vltmedia.com>
2025-01-31 22:19:39 -0500

    Feature: Added Llama Connector

---
+++ LlamaServer_Connector/LLamaServerConnector.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LlamaServer.Connector
+{
+    [System.Serializable]
+    public class LLamaServerConnector
+    {
+        public static string processName = "LlamaServer";
+        public static string processNameOS { get { 
+            return processName + GetAppExtension();
+            } }
+        public static int port = 5598;
+
+        public static string GetAppExtension()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return ".exe";
+            return "";
+        }
+        public static void CheckProcessRunning( Action onRunning, Action onNotRunning, string ProcessPath = "C:/Program Files/Llama Server/Llama Server/LlamaServer.exe")
+        {
+            ProcessMonitor processMonitor = new ProcessMonitor(processNameOS, () =>
+            {
+                port = ProcessMonitor.GetProcessPort(processName) ?? port;
+                onRunning();
+            }, () =>
+            {
+               if(System.IO.File.Exists(ProcessPath ))
+               {
+                   System.Diagnostics.Process.Start(ProcessPath);
+                    onRunning();
+               }
+               else
+               {
+                    onNotRunning();
+               }
+            }, 0);
+        }
+
+        public static async Task<string> SendPostRequestAsync(string url, string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Set request content type to JSON
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Send the request
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode(); // Throw if status code is not success
+
+                // Read response content
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        public static string SendUserInput(string userInput, string host = "http://localhost")
+        {
+            return SendUserInputAsync(userInput, host).Result;
+        }
+        public static async Task<string> SendUserInputAsync(string userInput, string host="http://localhost")
+        {
+            UserRequest userRequest = new UserRequest();
+            userRequest.UserInput = userInput;
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(userRequest);
+            string res = await SendPostRequestAsync(string.Format("{0}:{1}", host, port), json);
+            if (res != null) {
+                return res;
+            }
+            return "";
+        }
+
+        public static void KillProcess()
+        {
+            ProcessMonitor.KillProcessByName(processNameOS);
+        }
+        public class UserRequest
+        {
+            public string UserInput { get; set; } = "";
+        }
+
+
+    }
+}
---
+++ LlamaServer_Connector/ProcessMonitor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+public class ProcessMonitor
+{
+    private readonly string _processName;
+    private readonly Action _onRunning;
+    private readonly Action _onNotRunning;
+    private readonly Timer _timer;
+
+    public ProcessMonitor(string processName, Action onRunning, Action onNotRunning, int checkIntervalMs = 0)
+    {
+        _processName = processName;
+        _onRunning = onRunning;
+        _onNotRunning = onNotRunning;
+        if (checkIntervalMs > 0)
+        {
+            _timer = new Timer(CheckProcess, null, 0, checkIntervalMs);
+        }
+        else
+        {
+            CheckProcess(null);
+        }
+    }
+    public static int? GetProcessPort(string processName)
+    {
+        var process = Process.GetProcessesByName(processName).FirstOrDefault();
+        if (process == null)
+            return null;
+
+        int processId = process.Id;
+
+        var tcpConnections = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections();
+        foreach (var connection in tcpConnections)
+        {
+            if (connection.State == TcpState.Established && connection.LocalEndPoint.Port > 0)
+            {
+                var p = Process.GetProcesses().FirstOrDefault(x => x.Id == processId);
+                if (p != null)
+                    return connection.LocalEndPoint.Port;
+            }
+        }
+
+        return null;
+    }
+    public static void KillProcessByName(string processName)
+    {
+        var processes = Process.GetProcessesByName(processName);
+
+        if (processes.Length == 0)
+        {
+            Console.WriteLine($"No process named {processName} found.");
+            return;
+        }
+
+        foreach (var process in processes)
+        {
+            try
+            {
+                process.Kill();
+                process.WaitForExit(); // Ensures process is fully terminated
+                Console.WriteLine($"Killed process {process.ProcessName} (PID: {process.Id})");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not kill {process.ProcessName} (PID: {process.Id}): {ex.Message}");
+            }
+        }
+        }
+        private void CheckProcess(object state)
+    {
+        bool isRunning = Process.GetProcesses()
+                                .Any(p => p.ProcessName.Equals(_processName, StringComparison.OrdinalIgnoreCase));
+
+        if (isRunning) { 
+        _onRunning?.Invoke();
+    }
+        else{
+            _onNotRunning?.Invoke();
+}
+    }
+
+    public void Stop()
+    {
+        _timer.Dispose();
+    }
+}
